1f95ba9fac44afe0dcd9180fceab1fa02c52eeb1
Angry3vlibot <myh.balaker@gmail.com>
2022-12-13 00:19:20 +0000

    Create 'Free time' section and add content to 'About me' section
    - Created 'Free time' section
    - Added text to the 'Free time' section
    - Added images to the 'Free time' section
    - Added part of the logic for displaying images on text hover in the 'Free time' section
    - Added more text to the 'About me' section
    
    TODO: complete image display logic, fix overflow on image display

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,61 @@
 import BlazeSlider from 'blaze-slider'
 import 'blaze-slider/dist/blaze.css'
-let hider : number
-function showMeme(){
 
-    if(document.querySelector('#meme-container')?.classList.contains('hidden')){
+let hider: number
+function showMeme() {
+    if (document.querySelector('#meme-container')?.classList.contains('hidden')) {
         document.querySelector('#meme-container')?.classList.remove('hidden')
         clearTimeout(hider)
         hider = setTimeout(showMeme, 3000)
     }
-    else{
+    else {
         document.querySelector('#meme-container')?.classList.add('hidden')
         clearTimeout(hider)
     }
 }
-document.querySelector('#grievous')?.addEventListener('click', showMeme)
 
-const slider : any = document.querySelector('.blaze-slider')
+function showImage(ev: any) {
+    if(ev.type === 'mouseover'){
+        switch(ev.currentTarget.attributes[1].nodeValue){
+            case 'gaming':
+                ev.currentTarget.parentElement.parentElement.querySelector('.gaming').classList.remove('hidden')
+                break
+            case 'vr':
+                ev.currentTarget.parentElement.parentElement.querySelector('.vr').classList.remove('hidden')
+                break
+            case 'cycling':
+                ev.currentTarget.parentElement.parentElement.querySelector('.cycling').classList.remove('hidden')
+                break
+            case 'english':
+                ev.currentTarget.parentElement.parentElement.querySelector('.english').classList.remove('hidden')
+                break
+        }
+    }
+    else{
+        switch(ev.currentTarget.attributes[1].nodeValue){
+            case 'gaming':
+                ev.currentTarget.parentElement.parentElement.querySelector('.gaming').classList.add('hidden')
+                break
+            case 'vr':
+                ev.currentTarget.parentElement.parentElement.querySelector('.vr').classList.add('hidden')
+                break
+            case 'cycling':
+                ev.currentTarget.parentElement.parentElement.querySelector('.cycling').classList.add('hidden')
+                break
+            case 'english':
+                ev.currentTarget.parentElement.parentElement.querySelector('.english').classList.add('hidden')
+                break
+        }
+    }
+}
+
+document.querySelector('#grievous')?.addEventListener('click', showMeme)
+document.querySelector('#freetime')?.querySelectorAll('span').forEach((span) => {
+    span.addEventListener('mouseover', showImage)
+    span.addEventListener('mouseleave', showImage)
+})
+
+const slider: any = document.querySelector('.blaze-slider')
 new BlazeSlider(slider, {
     all: {
         loop: true,
@@ -23,13 +63,13 @@ new BlazeSlider(slider, {
         autoplayInterval: 2000,
         transitionDuration: 300,
         slidesToShow: 3,
-      },
-      '(max-width: 900px)': {
+    },
+    '(max-width: 900px)': {
         slidesToShow: 2,
-      },
-      '(max-width: 500px)': {
+    },
+    '(max-width: 500px)': {
         loop: true,
         slidesToShow: 1,
     },
 })
-export{}
+export { }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./src/**/*.{html,js}",
+
+  './*.{html,js}'],
   theme: {
     extend: {},
   },
